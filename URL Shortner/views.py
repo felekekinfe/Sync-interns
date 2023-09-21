6b0f72e62f0425afa0204b907a6bca176b355196
felekekinfe <felekekinfe@gmail.com>
2023-09-21 04:39:44 -0400

    commit

---
+++ URL Shortner/views.py
@@ -0,0 +1,20 @@
+import pyshorteners
+from django import render
+
+def shortner(request):
+    short_url=''
+    if request.method=='POST':
+        long_url=request.POST.get('longurl')
+
+        pyshort=pyshorteners.Shortener()
+        try:
+            short_url=pyshort.short(long_url)
+
+            return render(request,'index.html',{'short_url':short_url})
+        except:
+            short_url=''
+
+            return render(request,'index.html',{'short_url':short_url})
+    else:
+        return render(request,'index.html',{'short_url':short_url})
+
